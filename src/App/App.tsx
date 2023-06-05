1a36b605251024bd053f805ff711cc6c7bfd7dfe
ANDREI UDEANU <udeanugheorgheandrei@gmail.com>
2023-06-05 16:12:22 +0200

    Fixing Bugs

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pages from './Componets/Pages.tsx/Pages';
+
+const App: React.FC = () => {
+  return (
+  <>
+    <Pages />
+  </>
+  );
+}
+
+
+export default App;
---
+++ src/App/Componets/Pages.tsx/Pages.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Home from '../Home/Home';
+
+
+const Pages: React.FC = () => {
+  return (
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  </>
+  )
+}
+
+
+export default Pages;
