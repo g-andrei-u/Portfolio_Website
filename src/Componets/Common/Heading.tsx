3fef31d8342a18ae232d0874bc0d4757b193dcb0
ANDREI UDEANU <udeanugheorgheandrei@gmail.com>
2023-06-10 01:56:12 +0200

    Adding Icon

--- src/Componets/Common/Heading.tsx
+++ src/Componets/Common/Heading.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Heading.css';
 import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 
 const Heading: React.FC = () => {
+
+    const [hamburger, setHamburger] = useState(false);
+
+
     return (
         <header>
             <div className='heading'>
@@ -11,8 +16,18 @@ const Heading: React.FC = () => {
                     <h1>{`<Andrei Udeanu/>`}</h1>
                 </div>
                 <nav>
-                    <Link to={'/'}>HOME</Link>
-                    <Link to={'/projects'}>PROJECTS</Link>
+                    <div className='nav'>
+                        <Link to={'/'}>HOME</Link>
+                        <Link to={'/projects'}>PROJECTS</Link>
+                    </div>
+
+                    <div className='hamburger'>
+                        <button onClick={() => hamburger ? setHamburger(false) : setHamburger(true)}><GiHamburgerMenu /></button>
+                        <div className= {hamburger ? 'hamburger-bar' : 'nav'}>
+                            <Link to={'/'}>HOME</Link>
+                            <Link to={'/projects'}>PROJECTS</Link>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </header>
